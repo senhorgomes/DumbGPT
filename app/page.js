c2a3c820362866a716e66ac9fd691153c538c9fa
Bryan Gomes <bryanpgomes@gmail.com>
2024-05-04 22:18:54 -0400

    Initial new setup

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_CHATAPIKEY)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
